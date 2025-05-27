9a3609f41bb821d572027ff8f591ddafedccd44a
lsj0202 <w1004123@gmail.com>
2025-05-27 12:24:43 +0900

    feat: common button 컴포넌트 추가

---
+++ src/components/common/Button/OutlineButton.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import { forwardRef, type Ref } from 'react';
+
+import { colors } from '@/styles';
+
+import { type ButtonProps, buttonSizeStyle } from '.';
+
+const OutlineButton = forwardRef(
+  ({ children, size, type = 'button' }: ButtonProps, ref: Ref<HTMLButtonElement>) => {
+    return (
+      <StyledOutlineButton type={type} ref={ref} size={size}>
+        {children}
+      </StyledOutlineButton>
+    );
+  },
+);
+
+export default OutlineButton;
+
+const StyledOutlineButton = styled.button<ButtonProps>`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  padding: 0px 16px;
+  background-color: white;
+  cursor: pointer;
+  ${({ size }) => size && buttonSizeStyle[size]};
+  border: 1px solid ${colors.gray250};
+
+  &:hover {
+    background-color: ${colors.gray100};
+  }
+`;
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,8 +1,12 @@
 export const colors = {
-  primary: '#0064FF',
+  primary: '#1d78ff',
+  primaryHover: '#0066ff',
+  primaryDisabled: '#569aff',
 
   gray100: '#f0f0f0',
+  gray150: '#e4e4e4',
   gray200: '#cacaca',
+  gray250: '#5d5d5d',
   gray300: '#434343',
 } as const;
 
